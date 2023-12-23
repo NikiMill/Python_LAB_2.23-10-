6b3c3a5f0c3b1166c58a6b29d9ab964fe6690753
NikiMill <108574543+NikiMill@users.noreply.github.com>
2023-12-23 08:22:18 +0300

    Add files via upload

---
+++ prog/idz.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# С использованием многопоточности для заданного значения x найти сумму ряда S с точностью члена ряда по абсолютному
+# значению ε=10-7 и произвести сравнение полученной суммы с контрольным значением функции для двух бесконечных рядов.
+
+import threading
+import math
+
+class SeriesThread(threading.Thread):
+    def __init__(self, x, epsilon):
+        threading.Thread.__init__(self)  # Инициализация объекта потока
+        self.x = x # Значение x для ряда
+        self.epsilon = epsilon # Заданная точность для вычислений
+        self.result = 0 # Инициализация переменной для хранения суммы ряда
+
+    def run(self):
+        n = 0  # Инициализация переменной n (степень в ряде)
+        term = (self.x ** (2 * n)) / math.factorial(2 * n) # Вычисление первого члена ряда
+
+        # Вычисление суммы ряда до достижения заданной точности epsilon
+        while abs(term) > self.epsilon:
+            self.result += term
+            n += 1
+            term = (self.x ** (2 * n)) / math.factorial(2 * n)
+
+def main():
+    x = 1/2
+    epsilon = 1e-7
+
+    # Задаем контрольное значение
+    control_value = (math.exp(x) + math.exp(-x)) / 2
+
+    # Создаем поток для вычисления суммы ряда
+    series_thread = SeriesThread(x, epsilon)
+
+    # Запускаем поток
+    series_thread.start()
+
+    # Ждем завершения потока
+    series_thread.join()
+
+    # Получаем результат
+    series_sum = series_thread.result
+
+    # Сравниваем результат с контрольным значением
+    if abs(series_sum - control_value) < epsilon:
+        print(f"Сумма ряда: {series_sum}")
+        print(f"Контрольное значение: {control_value}")
+        print("Результат совпадает с контрольным значением.")
+    else:
+        print("Результат не совпадает с контрольным значением.")
+
+if __name__ == "__main__":
+    main()
+
